f768b7b6805d0157b4f4e8b12bb71d4eedc37a05
Mahmud Hasan <mahmud.buet.me@gmail.com>
2020-09-07 16:46:06 -0400

    minor edits to alexnet

--- loader_util/nn/conv/alexnet.py
+++ loader_util/nn/conv/alexnet.py
@@ -23,7 +23,7 @@ class AlexNet:
         if K.image_data_format() == "channel_first":
             inputShape = depth, height, width
             chanDim = 1
-
+        ###############################################################################
         # Block1: CONV-RELU-POOL
         model.add(Conv2D(filters=96,
                          kernel_size=(11, 11),
@@ -35,7 +35,7 @@ class AlexNet:
         model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
         model.add(Dropout(0.25))
-
+        ###############################################################################
         # Block2: CONV-RELU-POOL
         model.add(Conv2D(filters=256,
                          kernel_size=(5, 5),
@@ -45,7 +45,7 @@ class AlexNet:
         model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
         model.add(Dropout(0.25))
-
+        ###############################################################################
         # Block #3: CONV => RELU => CONV => RELU => CONV => RELU
         model.add(Conv2D(384, (3, 3), padding="same",
                          kernel_regularizer=l2(reg)))
@@ -61,20 +61,20 @@ class AlexNet:
         model.add(BatchNormalization(axis=chanDim))
         model.add(MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
         model.add(Dropout(0.25))
-
+        ###############################################################################
         # Block #4: first set of FC => RELU layers
         model.add(Flatten())
         model.add(Dense(4096, kernel_regularizer=l2(reg)))
         model.add(Activation("relu"))
         model.add(BatchNormalization())
         model.add(Dropout(0.5))
-
+        ###############################################################################
         # Block #5: second set of FC => RELU layers
         model.add(Dense(4096, kernel_regularizer=l2(reg)))
         model.add(Activation("relu"))
         model.add(BatchNormalization())
         model.add(Dropout(0.5))
-
+        ###############################################################################
         # softmax classifier
         model.add(Dense(classes, kernel_regularizer=l2(reg)))
         model.add(Activation("softmax"))
