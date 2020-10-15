fb78c54477310a717789d54d0df29ce78f59f5db
Mahmud Hasan <mahmud.buet.me@gmail.com>
2020-10-15 03:54:32 -0400

    removed tahsins project

--- ml/moodring/feat_agg.py
+++
@@ -1,8 +0,0 @@
-def feat_agg(feat_output, feat_imp, dict_feat):
-    for i, feat in enumerate(feat_output):
-        if feat in dict_feat.keys():
-            dict_feat[feat].append(feat_imp[i])
-        else:
-            dict_feat[feat] = [feat_imp[i]]
-
-    return dict_feat
--- ml/moodring/performance.py
+++
@@ -1,9 +0,0 @@
-def performance(pred_degree, test_y_degree):
-    if pred_degree == test_y_degree:
-        perform = 'good'
-    elif pred_degree > test_y_degree:
-        perform = 'higher'
-    elif pred_degree < test_y_degree:
-        perform = 'lower'
-
-    return perform
