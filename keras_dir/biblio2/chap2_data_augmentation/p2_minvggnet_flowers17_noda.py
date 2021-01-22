b6f8e5c99d4fcf1d5c683f1c261a1aca5998d7f3
Mahmud Hasan <mahmud.buet.me@gmail.com>
2021-01-22 17:28:37 -0500

    redoing biblio2 dl4cv, data augmentation

--- keras_dir/biblio2/chap2_data_augmentation/p2_minvggnet_flowers17_noda.py
+++ keras_dir/biblio2/chap2_data_augmentation/p2_minvggnet_flowers17_noda.py
@@ -1,110 +1,89 @@
-# import the required packages
-# import the necessary packages
+# %%
+# import the necessary keras packages
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from loader_util.preprocessing import ImageToArrayPreprocessor, \
     AspectAwarePreprocessor
 from loader_util.datasets import SimpleDatasetLoader
-from loader_util.nn.conv import FCHeadNet
+from loader_util.nn.conv import FCHeadNet, MinVGGNet
 ##
 from tensorflow.keras.layers import Conv2D, Activation, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import RMSprop, SGD
-from loader_util.nn.conv import MinVGGNet
 from tensorflow.keras.applications import VGG16
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
 from tensorflow.keras import backend as K
 from imutils import paths
+import seaborn as sns
 import numpy as np
 import os
 
 # %%
 
-# construct the imagepaths
-data_dir = r"C:\Users\mhasa\Google Drive\Tutorial " \
-           r"Corner\PYTH\DeepLearning\DeepLearning-DL4CV\ImageDatasets\flowers17\images"
+data_dir = r'C:\Users\mhasa\Google Drive\Tutorial Corner\PYTH\DeepLearning\DeepLearning-DL4CV\ImageDatasets\flowers17\images'
 
-# grab the list of images then extract class labels
-print(f"[INFO] loading images.....")
-image_paths = list(paths.list_images(basePath=data_dir))
-class_names = [pt.split(sep=os.path.sep)[-2] for pt in image_paths]
-class_names = [str(x) for x in np.unique(class_names)]
-
+# grab the list of images
+print(f'[INFO] loading images.....')
+image_paths = list(paths.list_images(data_dir))
+class_names = [pt.split(os.path.sep)[-2] for pt in image_paths]
+unique_class_names = np.unique(class_names)
+total_classes = len(unique_class_names)
 # %%
 
-
-# initlaise the image preprocessors
+# initialise the preprocessors
 aap = AspectAwarePreprocessor(width=64, height=64)
 iap = ImageToArrayPreprocessor()
-
-# load the dataset from disk and then scale the pixel intensities [0, 1]
+# load the dataset from disk and then scale raw pixels to [0-1]
 sdl = SimpleDatasetLoader(preprocessors=[aap, iap])
-data, labels = sdl.load(image_paths, verbose=500)
-data = data.astype("float") / 255.0
+data, labels = sdl.load(image_paths,
+                        verbose=500)  # type: np.ndarray, np.ndarray
+data = data.astype('float') / 255.0
 # %%
 
-# partition the data into training and test sets
-trainx, testx, trainy, testy = train_test_split(data, labels,
+# partition data into train and test splits
+trainx, testx, trainy, testy = train_test_split(data,
+                                                labels,
                                                 test_size=0.25,
                                                 random_state=42)
-# convert the labels from the integers
-le = LabelBinarizer()
-trainy = le.fit_transform(trainy)
-testy = le.transform(testy)
-
-#%%
-
-# construct the image generator
-aug = ImageDataGenerator(rotation_range=30,
-                         width_shift_range=0.1,
-                         height_shift_range=0.1,
-                         shear_range=0.2,
-                         zoom_range=0.2,
-                         horizontal_flip=True,
-                         fill_mode='nearest')
-
-
+# convert labels to nos
+lb = LabelBinarizer()
+trainy = lb.fit_transform(trainy)
+testy = lb.transform(testy)
 # %%
 
-# initialize the optimizer and the model
-print(f"[INFO] compiling model.....")
+# init the optimizer and model
+print(f'[INFO] compiling model.....')
 opt = SGD(lr=0.05)
-model = MinVGGNet.build(width=64, height=64, depth=3, classes=len(class_names))
+model = MinVGGNet.build(width=64,
+                        height=64,
+                        depth=3,
+                        classes=total_classes)
+
 model.compile(loss="categorical_crossentropy",
               optimizer=opt,
               metrics=["accuracy"])
 # %%
 
-# train the network
-print(f"[INFO] training the network .....")
-H = model.fit_generator(aug.flow(trainx, trainy, batch_size=32),
-                        validation_data=(testx, testy),
-                        steps_per_epoch=len(trainx) // 32,
-                        epochs=100,
-                        verbose=1)
+print(f'[INFO] training network.....')
+H = model.fit(trainx, trainy, validation_data=(testx, testy),
+              batch_size=32, epochs=100, verbose=1)
 # %%
-
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testx, batch_size=32)
 print(classification_report(testy.argmax(axis=1),
                             predictions.argmax(axis=1),
-                            target_names=class_names))
-
-# %%
-
+                            target_names=unique_class_names))
+#%%
 # plot the performance
-epochs = range(1, 101)
+epochs = range(1, 41)
 loss = H.history['loss']
-accuracy = H.history['accuracy']
+accuracy = H.history['acc']
 val_loss = H.history['val_loss']
-val_accuracy = H.history['val_accuracy']
+val_accuracy = H.history['val_acc']
 plot_df = pd.DataFrame(
     data=np.c_[epochs, loss, accuracy, val_loss, val_accuracy],
     columns=['epochs', 'loss', 'accuracy', 'val_loss', 'val_accuracy'])
@@ -122,6 +101,4 @@ sns.lineplot(data=plot_df, x='epochs', y='val_accuracy', ax=ax,
              label='val_accuracy', linewidth=3)
 ax.set_ylabel('Loss or Accuracy')
 ax.set_xlabel('Epochs')
-plt.setp(ax.get_legend().get_texts(), fontsize='18');  # for legend text
-plt.show()
-#%%
+plt.setp(ax.get_legend().get_texts(), fontsize='18');  # for legend textr
