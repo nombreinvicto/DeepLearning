32b7f5bdb828d3a9ac2156a12cb6060e95459e9b
nombreinvicto <mahmud.buet.me@gmail.com>
2021-10-06 01:19:07 -0600

    recoded resnet.py

--- cv_playground.py
+++ cv_playground.py
@@ -1,23 +1,63 @@
-import torch
-from loader_util.nn.conv import LeNetTorch
-from torchsummary import summary
-
-model = LeNetTorch(classes=50)
-summary(model, input_size=(3, 224, 224))
-
-import numpy as np
 # %%
-from functools import reduce
-def apply_to_layer(layer):
-    if isinstance(layer, torch.nn.Conv2d):
-        n = reduce(lambda x, y: x * y, layer.weight.shape)
-    elif isinstance(layer, torch.nn.Linear):
-        n = layer.in_features
-    else:
-        return
-    y = 1.0 / (np.sqrt(n))
-    torch.nn.init.normal_(layer.weight, mean=0, std=y)
+# import the necessary packages
+import inspect
+#%%
+from imutils import paths
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from loader_util.preprocessing import ImageToArrayPreprocessor, \
+    AspectAwarePreprocessor, SimplePreProcessor, MeanPreprocessor
+from loader_util.io import HDF5DatasetGenerator
+from loader_util.datasets import SimpleDatasetLoader
+from loader_util.nn.conv import FCHeadNet
+from loader_util.callbacks import EpochCheckpoint, TrainingMonitor
+##
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Conv2D, Activation, Flatten, Dense, \
+    BatchNormalization, MaxPooling2D, AveragePooling2D, Dropout
+from tensorflow.keras.layers import Input, concatenate
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop, SGD
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.models import Model, load_model
+
+# %%
+
+# ground truth
+true_weights = tf.constant(list(range(5)), dtype=tf.float32)
+true_weights = tf.reshape(true_weights, shape=(true_weights.shape[0], -1))
+# %%
+
+# some random trianing data
+x = tf.constant(tf.random.uniform((32, 5)), dtype=tf.float32)
+y = tf.constant(x @ true_weights, dtype=tf.float32)
+# %%
+
+# model parameters
+weights = tf.Variable(tf.random.uniform((5, 1)), dtype=tf.float32)
+# %%
+for it in range(1001):
+    with tf.GradientTape() as tape:
+        y_hat = tf.linalg.matmul(x, weights)
+        loss = tf.reduce_mean(tf.square(y - y_hat))
+
+    if not it % 100:
+        print(f"[INFO] mean squared loss at iteration: {it} is {loss}......")
+
+    gradients = tape.gradient(loss, weights)
+    weights.assign_add(-0.05 * gradients)
 
 
 # %%
-model.apply(apply_to_layer)
+
+
+def f(a, b, power=2, d=3):
+    return tf.pow(a, power) + d * b
+
+
+converted_f = tf.autograph.to_graph(f)
+print(inspect.getsource(converted_f))
+#%%
--- loader_util/nn/conv/resnet.py
+++ loader_util/nn/conv/resnet.py
@@ -1,83 +1,83 @@
-import sys, os, json
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import argparse, progressbar
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-import matplotlib.axes._axes as axes
-
-sns.set()
 # %%
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Conv2D, Activation, Flatten, Dense, \
-    BatchNormalization, MaxPooling2D, AveragePooling2D, Dropout
-from tensorflow.keras.layers import Input, concatenate, add
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import RMSprop, SGD
+    BatchNormalization, MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Input, add
 from tensorflow.keras.regularizers import l2
-from tensorflow.keras.applications import VGG16
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 
 
 # %%
 
-class Resnet:
+class ResNet:
     @staticmethod
-    def residual_module(data, K, stride, chand_dim, red=False,
-                        reg=0.0001, bn_eps=2e-5, bn_mom=0.9):
+    def residual_module(data, K, stride, chan_dim, red=False, reg=0.0001, bn_eps=2e-5, bn_mom=0.9):
+        # shortcut or identity branch
         shortcut = data
-
-        # first block of 1x1 conv
-        bn1 = BatchNormalization(axis=chand_dim, epsilon=bn_eps,
-                                 momentum=bn_mom)(data)
+        # first block = BN + ReLU + Conv
+        bn1 = BatchNormalization(axis=chan_dim, momentum=bn_mom, epsilon=bn_eps)(data)
         act1 = Activation('relu')(bn1)
-        conv1 = Conv2D(filters=int(K / 4), kernel_size=(1, 1),
-                       use_bias=False, kernel_regularizer=l2(reg))(act1)
+        conv1 = Conv2D(filters=(K // 4), kernel_size=(1, 1), strides=(1, 1), use_bias=False, kernel_regularizer=l2(reg))(act1)
 
-        # second block of 3x3
-        bn2 = BatchNormalization(axis=chand_dim, epsilon=bn_eps,
-                                 momentum=bn_mom)(conv1)
+        # second block BN + ReLU + Conv
+        bn2 = BatchNormalization(axis=chan_dim, momentum=bn_mom, epsilon=bn_eps)(conv1)
         act2 = Activation('relu')(bn2)
-        conv2 = Conv2D(filters=int(K / 4), kernel_size=(3, 3),
-                       strides=stride, padding='same',
-                       use_bias=False, kernel_regularizer=l2(reg))(act2)
+        conv2 = Conv2D(filters=(K // 4), kernel_size=(3, 3), strides=stride, padding="same", use_bias=False, kernel_regularizer=l2(reg))(act2)
 
-        # third block of 1x1
-        bn3 = BatchNormalization(axis=chand_dim, epsilon=bn_eps,
-                                 momentum=bn_mom)(conv2)
+        # third block
+        bn3 = BatchNormalization(axis=chan_dim, epsilon=bn_eps, momentum=bn_mom)(conv2)
         act3 = Activation('relu')(bn3)
-        conv3 = Conv2D(filters=K, kernel_size=(1, 1), use_bias=False,
-                       kernel_regularizer=l2(reg)(act3))
+        conv3 = Conv2D(filters=K, kernel_size=(1, 1), use_bias=False, kernel_regularizer=l2(reg))(act3)
 
         if red:
-            shortcut = Conv2D(filters=K, kernel_size=(1, 1), strides=stride,
-                              use_bias=False, kernel_regularizer=l2(reg)(act1))
+            shortcut = Conv2D(filters=K, kernel_size=(1, 1), strides=stride, use_bias=False, kernel_regularizer=l2(reg))(shortcut)
 
-        # add together the shortcut and final CONV
-        x = add([conv3, shortcut])
-        return x
+        return add([conv3, shortcut])
 
     @staticmethod
-    def build(width, height, depth, classes, stages: list, filters: list,
-              reg=0.0001, bn_eps=2e-5, bn_mom=0.9, dataset='cifar'):
+    def build(width, height, depth, classes, stages=(), filters=(), reg=0.0001, bn_eps=2e-5, bn_mom=0.9, dataset="cifar"):
+        # init the input shape
         input_shape = height, width, depth
         chan_dim = -1
 
-        if K.image_data_format() == 'channel_first':
+        if K.image_data_format() == "channel_first":
             input_shape = depth, height, width
             chan_dim = 1
 
-        # set input and apply BN
+        # set input
         inputs = Input(shape=input_shape)
-        x = BatchNormalization(axis=chan_dim,
-                               epsilon=bn_eps,
-                               momentum=bn_mom)(inputs)
-        if dataset == 'cifar':
-            # apply single CONV layer
-            x = Conv2D(filters=filters[0], kernel_size=(3, 3), use_bias=False,
-                       padding='same', kernel_regularizer=l2(reg))(x)
+        x = BatchNormalization(axis=chan_dim, epsilon=bn_eps, momentum=bn_mom)(inputs)
 
-            for i in range(len(stages)):
-                pass
+        if dataset == "cifar":
+            x = Conv2D(filters=filters[0], kernel_size=(3, 3), use_bias=False, padding='same', kernel_regularizer=l2(reg))(x)
+
+        # loop over the stages
+        for i in range(len(stages)):
+            stride = (1, 1) if i == 0 else (2, 2)
+            x = ResNet.residual_module(data=x, K=filters[i + 1], stride=stride, chan_dim=chan_dim, red=True, bn_eps=bn_eps, bn_mom=bn_mom)
+
+            # loop  over the number of j stages for each stage
+            for j in range(stages[i] - 1):
+                # create more resnet modules without reduction
+                x = ResNet.residual_module(data=x, K=filters[i + 1], stride=(1, 1), chan_dim=chan_dim, bn_eps=bn_eps, bn_mom=bn_mom)
+
+        # apply BN -> ACT -> POOL
+        x = BatchNormalization(axis=chan_dim, epsilon=bn_eps, momentum=bn_mom)(x)
+        x = Activation('relu')(x)
+        x = AveragePooling2D(pool_size=(8, 8))(x)
+
+        # softmax
+        x = Flatten()(x)
+        x = Dense(classes, kernel_regularizer=l2(reg))(x)
+        x = Activation('softmax')(x)
+
+        # create model
+        model = Model(inputs=inputs, outputs=x, name="resnet")
+        return model
+
+
+if __name__ == '__main__':
+    my_model = ResNet.build(width=32, height=32, depth=3, classes=10, stages=(3, 4, 6), filters=(64, 128, 256, 512))
+    print(my_model.summary())
+# %%
