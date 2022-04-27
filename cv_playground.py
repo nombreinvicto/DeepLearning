a832697b5697ac0966cd0cda534c7ed98be60e41
nombreinvicto <mahmud.buet.me@gmail.com>
2022-04-26 20:20:10 -0600

    added pytorch mnist ann and linalg chap2

--- cv_playground.py
+++ cv_playground.py
@@ -13,3 +13,50 @@ image = cv2.imread(image_path)
 for image in image_pyramid(image):
     cv2.imshow('slice', image)
     cv2.waitKey(0)
+
+# evaluate the network
+print ( "[INFO] evaluating network..." )
+predictions = model.predict(testx, batch_size=32 )
+print (classification_report(testy.argmax(axis=1 ),
+predictions.argmax(axis=1 ), target_names=classNames))
+
+
+# plot the performance
+epochs = range(1,41)
+loss = H.history['loss']
+accuracy = H.history['acc']
+val_loss = H.history['val_loss']
+val_accuracy = H.history['val_acc']
+plot_df = pd.DataFrame(data=np.c_[epochs,loss,accuracy,val_loss,val_accuracy],
+                       columns=['epochs','loss', 'accuracy', 'val_loss', 'val_accuracy'])
+
+# do the actual plots
+sns.set(font_scale=1)
+f, ax = plt.subplots(1, 1, figsize=(15,8))
+sns.lineplot(data=plot_df, x='epochs', y='loss', ax=ax, label='train loss', linewidth=3)
+sns.lineplot(data=plot_df, x='epochs', y='accuracy', ax=ax, label='train accuracy', linewidth=3)
+sns.lineplot(data=plot_df, x='epochs', y='val_loss', ax=ax, label='val loss', linewidth=3)
+sns.lineplot(data=plot_df, x='epochs', y='val_accuracy', ax=ax, label='val_accuracy', linewidth=3)
+ax.set_ylabel('Loss or Accuracy')
+ax.set_xlabel('Epochs')
+plt.setp(ax.get_legend().get_texts(), fontsize='18'); # for legend text
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(all_true_targets,all_predictions)
+def print_confusion_matrix(confusion_matrix, class_names, figsize = (10,7), fontsize=14):
+
+    df_cm = pd.DataFrame(confusion_matrix, index=class_names, columns=class_names)
+    fig = plt.figure(figsize=figsize)
+
+    try:
+        heatmap = sns.heatmap(df_cm, annot=True, fmt="d", cmap='viridis')
+    except ValueError:
+        raise ValueError("Confusion matrix values must be integers")
+
+    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=fontsize)
+    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=fontsize)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
