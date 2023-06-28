20897cad62917b0673f3f6443bf66fbdd8aa9c64
Mahmud Hasan <mahmud.buet.me@gmail.com>
2023-06-28 00:08:04 -0600

    updated resnet dir

--- cv_playground.py
+++ cv_playground.py
@@ -1,3 +1,13 @@
-import sys
+import time
 
-print(sys.executable)
+import sys
+from flask import Flask
+from collections import Counter
+from sklearn.naive_bayes import MultinomialNB
+
+# %% ##################################################################
+while True:
+    print(f"[INFO] hello world......")
+    time.sleep(1)
+
+
