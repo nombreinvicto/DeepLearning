7f62b0b8913f20495b57e3349ffa66fd297cb875
Mahmud Hasan <mahmud.buet.me@gmail.com>
2025-06-23 01:51:07 -0600

    update commit

--- cv_playground.py
+++
@@ -1,9 +0,0 @@
-import sklearn
-import pandas as pd
-import tensorflow as tf
-
-print(sklearn.__version__)
-print(pd.__version__)
-print(tf.__version__)
-
-# %% ##################################################################
