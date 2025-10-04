e62c588433888281540dfce29b934378b97195fa
Mahmud Hasan <mahmud.buet.me@gmail.com>
2025-10-04 04:40:32 -0600

    update commit

--- misc/misc/apu_research/multi_scatter.R
+++ misc/misc/apu_research/multi_scatter.R
@@ -2,13 +2,13 @@ library(ggplot2)
 library(readr)
 setwd("C:\\GoogleDrive\\GitHub\\deep_learning\\misc\\misc\\apu_research")
 df = read.csv('summary_stats_sovi_svi_LI_dynamic0711.csv')
-df$LI='Low Income'
+df$LI = 'Low Income'
 df2 = read.csv('summary_stats_sovi_svi_NLI_dynamic0711.csv')
-df2$LI='not-Low Income'
-df=rbind(df,df2)
+df2$LI = 'not-Low Income'
+df = rbind(df, df2)
 
-x=df[df$Source=='SOVI',c('cliff_d','LI','Cregion')]
-y=df[df$Source=='SVI',c('cliff_d','LI','Cregion')]
+x = df[df$Source == 'SOVI', c('cliff_d', 'LI', 'Cregion')]
+y = df[df$Source == 'SVI', c('cliff_d', 'LI', 'Cregion')]
 
 
 library(ggplot2)
@@ -20,12 +20,12 @@ P <- list()
 print(unique(df$Source))
 for (zz in unique(df$Source)) {
   if (zz != 'SOVI' & zz != 'SVI') {
-    
+
     z <- df[df$Source == zz, c('cliff_d', 'LI', 'Cregion')]
     df1 <- merge(x, y, by = c('Cregion', 'LI'))
     df1 <- merge(df1, z, by = c('Cregion', 'LI'))
     names(df1) <- c('Cregion', 'Li', 'SOVI', 'SVI', 'J40')
-    
+
     # Create each plot without individual legends
     p <- ggplot(df1, aes(x = SOVI, y = SVI)) +
       annotate("rect", xmin = 0, xmax = 1, ymin = 0, ymax = 1,
@@ -41,14 +41,15 @@ for (zz in unique(df$Source)) {
         name = "Cliff's δ (J40)",
         breaks = c(-1, 0, 1),
         labels = c("Higher in \n independent", "Equal", "Higher in \n compound")
-      )+
+      ) +
       scale_shape_manual(values = c(16, 17)) +
-      xlim(c(-1, 1)) + ylim(c(-1, 1)) +
+      xlim(c(-1, 1)) +
+      ylim(c(-1, 1)) +
       scale_x_continuous(limits = c(-1, 1), expand = expansion(mult = 0.1)) +
-      scale_y_continuous(limits = c(-1, 1), expand = expansion(mult = 0.1))+
+      scale_y_continuous(limits = c(-1, 1), expand = expansion(mult = 0.1)) +
       geom_text(aes(label = Cregion), hjust = -0.2, vjust = -0.2, size = 2.5) +
       #theme_minimal() +
-      theme(panel.background = element_rect(fill = "white", colour = "grey50"))+
+      theme(panel.background = element_rect(fill = "white", colour = "grey50")) +
       labs(
         title = zz,
         x = "Cliff's δ (SOVI)", y = "Cliff's δ (SVI)", shape = "Income"
@@ -57,9 +58,9 @@ for (zz in unique(df$Source)) {
             plot.title = element_text(size = 9),  # 🔹 Reduce plot title size
             axis.title = element_text(size = 8)                  # Optional: axis labels
             #axis.text = element_text(size = 8)                    # Optional: axis tick labels
-            
+
       )
-    
+
     P[[zz]] <- p
   }
 }
@@ -88,7 +89,7 @@ legend <- get_legend(
         override.aes = list(size = 4)
       )
     )
-) 
+)
 
 
 # Add the legend to the 16th slot (bottom-right of 4x4 grid)
